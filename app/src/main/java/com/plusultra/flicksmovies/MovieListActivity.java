264f31f80a7379d8d9d6615a00d6cc26ee985db8
X Norman <xnorman@fb.com>
2018-06-28 14:11:05 -0700

    Adds basic functionality. Shows the ImageView block with the movie titles and overview

--- app/src/main/java/com/plusultra/flicksmovies/MovieListActivity.java
+++ app/src/main/java/com/plusultra/flicksmovies/MovieListActivity.java
@@ -1,17 +1,22 @@
 package com.plusultra.flicksmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+import com.plusultra.flicksmovies.models.Movie;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -30,6 +35,12 @@ public class MovieListActivity extends AppCompatActivity {
     String imageBaseUrl;
     //poster size
     String posterSize;
+    //currently playing movies
+    ArrayList<Movie> movies;
+    //the recycler view
+    RecyclerView rvMovies;
+    //the adapter ired to the recycler view
+    MovieAdapter adapter;
 
 
     @Override
@@ -38,8 +49,51 @@ public class MovieListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_list);
         //initialize the client
         client = new AsyncHttpClient();
+        //initialize the array list
+        movies = new ArrayList<>();
+        // initialize adapter -- movies array cannot be reinitialized after this point
+        adapter = new MovieAdapter(movies);
+
+        //resolve recycler view and connect to a layout manager and the adapter
+        rvMovies = (RecyclerView) findViewById(R.id.rvMovies);
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
+        rvMovies.setAdapter(adapter);
+
 
         getConfiguration();
+    }
+    //get list of currently playing films from the API
+    private void getNowPlaying() {
+        //create the URL
+        String url = API_BASE_URL + "/movie/now_playing";
+        //set request params
+        RequestParams params = new RequestParams();
+        params.put(API_KEY_PARAM, getString(R.string.api_key)); //API KEY always required
+        // execute GET request expecting a JSON response
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                //load the results in the into the movies list
+                try {
+                    JSONArray results = response.getJSONArray("results");
+                    //iterate through result set and Movie objects
+                    for (int i = 0; i < results.length(); i++) {
+                        Movie movie = new Movie(results.getJSONObject(i));
+                        movies.add(movie);
+                        //notify the adapter that a row was added
+                        adapter.notifyItemInserted(movies.size() - 1);
+                    }
+                    Log.i(TAG, String.format("Loaded %s movies", results.length()));
+                } catch (JSONException e) {
+                    logError("Failed to parse now playing movies", e, true);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                logError("Failed to get data from now_playing endpoint", throwable, true);
+            }
+        });
     }
 
     private void getConfiguration() {
@@ -51,18 +105,20 @@ public class MovieListActivity extends AppCompatActivity {
         client.get(url, params, new JsonHttpResponseHandler() {
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                super.onFailure(statusCode, headers, responseString, throwable);
+                logError("Failed getting configuration", throwable, true);
             }
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                super.onSuccess(statusCode, headers, response);
 
                 try {
                     JSONObject images = response.getJSONObject("images");
                     imageBaseUrl = images.getString("secure_base_url");
-                    JSONArray posterSizeOptions = response.getJSONArray("poster_sizes");
+                    JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
                     posterSize = posterSizeOptions.optString(3, "w342");
+                    Log.i(TAG, String.format("Loaded configuration with imageBaseUrl %s and posterSize %s", imageBaseUrl, posterSize));
+                    //get the now playing movie list
+                    getNowPlaying();
                 } catch (JSONException e) {
                     logError("Failed parsing configuration", e, true);
                 }
